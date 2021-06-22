2ad9b19b1634c258463c6d122576662cb2803c5b
JonasParringer <jonasparringer@gmx.de>
2021-06-22 12:55:31 +0200

    Projektdateien hinzufügen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_06_22_Fileupload.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
