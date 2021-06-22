8adef4e99fa0f5db82ac20ceda181e1d66ab9ea4
JonasParringer <jonasparringer@gmx.de>
2021-06-22 13:35:55 +0200

    Interface erstellt. Klappt aber noch nicht

---
+++ Models/Interface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2021_06_22_Fileupload.Models
+{
+    public interface IFromFile
+    {
+        string ContentType { get; }
+        string ContentDispoition { get; }
+        IHeaderDirectory Headers { get; }
+        long Length { get; }
+        string Name { get; }
+        string Filename { get; }
+        Stream OpenReadStream();
+        void CopyTo(Stream target);
+        Task CopyToAsync(Stream target, CancellationToken cancellationToken = null);
+
+    }
+}
